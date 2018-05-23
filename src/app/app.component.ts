8606cbe254872ae4057000a99817e591f07cf191
Naji Louis <naji@anghami.com>
2018-05-23 20:36:20 +0300

    test ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'eliane\'s app';
+  newInput : string = '';
+  myarr: Array<any>;
+
+  constructor(){    
+    let localArr = localStorage.getItem('starter-list')?localStorage.getItem('starter-list'):'[]';
+    this.myarr = JSON.parse(localArr);
+  }
+  
+
+  add(){
+    console.log(this.newInput);
+    if(this.newInput){
+      this.myarr.push({id : this.myarr.length + 1,name:this.newInput});
+      this.newInput = '';
+      localStorage.setItem('starter-list', JSON.stringify(this.myarr));
+    }
+  }
+
+  remove(index){
+    console.log(index)
+    this.myarr.splice(index,1);
+    localStorage.setItem('starter-list', JSON.stringify(this.myarr));
+  }
 }
